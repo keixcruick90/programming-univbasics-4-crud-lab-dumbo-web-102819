3382e22f9f88ec8573080002bcaeb732831e72e9
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-10-08 14:30:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  time_of_day = ["pm", "am"]
+  time_of_day[2]
 end
 
 def retrieve_first_element_from_array(array)
